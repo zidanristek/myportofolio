5c73715f74f6b0aa7763a7439dc50a0547a09d60
Muhammad Sultan Zidan <275972960+zidanristek@users.noreply.github.com>
2026-09-01 03:07:43 +0700

    chore(settings): register templates and static directories

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'portofolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 # Email
