913adbfcf27525d364fc63461d75568a7f9dbb0a
Muhammad Sultan Zidan <275972960+zidanristek@users.noreply.github.com>
2026-09-14 12:11:53 +0700

    build: pin django to 5.2 and drop unused deps
    Tutorial 0 menetapkan django~=5.2, tapi requirements.txt menulis django
    tanpa batas versi sehingga lingkungan lokal terpasang 6.1 dan PWS ikut
    memasang versi yang sama saat build. Perbedaan versi antara yang dipakai
    mata kuliah dan yang dipakai proyek ini tidak ada untungnya.
    
    requests dan urllib3 ikut dibuang karena tidak pernah diimpor di mana
    pun dan tidak ada dalam daftar Tutorial 0. Tautan dokumentasi di berkas
    bawaan startproject ikut diarahkan ke 5.2 supaya tidak menyesatkan.

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -2,7 +2,7 @@
 URL configuration for the portofolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
 """
 
 from django.contrib import admin
